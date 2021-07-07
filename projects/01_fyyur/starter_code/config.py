3fdddb70379034832e58f00430b20c2432475a4a
Alessandro Henrique <alessandrohenriqueho@gmail.com>
2021-07-07 15:17:40 -0300

    Get config data from env

--- projects/01_fyyur/starter_code/config.py
+++ projects/01_fyyur/starter_code/config.py
@@ -4,8 +4,13 @@ SECRET_KEY = os.urandom(32)
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 # Enable debug mode.
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True')
 
 # Connect to the database
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres@localhost:5432/fyyur'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
+DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
+DB_USER = os.getenv('DB_USER', 'postgres')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')
+DB_NAME = os.getenv('DB_NAME', 'fyyur')
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://{}:{}@{}/{}'.format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
+SQLALCHEMY_TRACK_MODIFICATIONS = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS', 'False')
